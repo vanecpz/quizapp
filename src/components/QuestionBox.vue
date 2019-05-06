72ba798824c94e1302d97c414a52176eeece6c56
Vanessa Parga <vcparga@worten.es>
2019-05-06 15:28:38 +0200

    squash

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template slot="lead">
+                {{ currentQuestion.question }}
+            </template>
+
+            <hr class="my-4">
+
+            <b-list-group>
+                <b-list-group-item
+                    v-for="(answer, index) in answers"
+                    :key="index"
+                    @click="selectAnswer(index)"
+                    :class="answerClass(index)"
+                >
+                    {{answer}}
+                </b-list-group-item>
+            </b-list-group>
+
+            <b-button variant="primary"
+            @click="submitAnswer"
+            :disabled="selectedIndex === null || answered"
+            >Submit</b-button>
+            <b-button variant="success" @click="next" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+   import _ from 'lodash'
+
+   export default {
+       props: {
+           currentQuestion: Object,
+           next: Function,
+           increment: Function
+       },
+       data() {
+           return {
+               selectedIndex: null,
+               shuffledAnswers: [],
+               correctIndex: null,
+               answered: false
+           }
+       },
+       computed: {
+            answers() {
+                let answers = [...this.currentQuestion.incorrect_answers]
+                answers.push(this.currentQuestion.correct_answer)
+
+                return answers
+            }
+       },
+       watch: {
+           currentQuestion: {
+                inmediate: true,
+                handler() {
+                    this.selectedIndex = null
+                    this.answered = false
+                    this.shuffleAnswers()
+                }
+           }
+       },
+       methods: {
+           selectAnswer(index) {
+               this.selectedIndex = index
+           },
+           submitAnswer() {
+                let isCorrect = false
+
+                if (this.selectedIndex === this.correctIndex) {
+                   isCorrect = true
+                }
+
+                this.increment(isCorrect)
+                this.answered = true
+
+           },
+           shuffleAnswers() {
+                let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+                this.shuffledAnswers = _.shuffle(answers)
+                console.log('Correcta:', this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer))
+                this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+           },
+           answerClass(index) {
+               let answerClass = ''
+
+               if (!this.answered && this.selectedIndex === index) {
+                   return 'selected'
+               }
+               if (this.answered && this.correctIndex === index) {
+                   return 'correct'
+               }
+               if (this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+                  return 'incorrect'
+               }
+
+           }
+       },
+       mounted() {
+           this.shuffleAnswers()
+       }
+   }
+</script>
+
+<style scoped>
+    .list-group {
+       margin-bottom: 15px;
+    }
+    .list-group-item:hover {
+       background: #EEE;
+       cursor: pointer;
+    }
+    .btn {
+       margin: 0 5px;
+    }
+    .selected {
+        background-color: lightblue;
+    }
+    .correct {
+        background-color: lightgreen;
+    }
+    .incorrect {
+        background-color: lightcoral;
+    }
+</style>
